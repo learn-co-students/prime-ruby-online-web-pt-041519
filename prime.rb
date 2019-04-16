ee2b7fa45d4727ae9e35a0de84ed64ab4528b095
Craig Donavin <crdonavin@gmail.com>
2019-04-16 21:09:05 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,22 @@
 # Add  code here!
+def prime?(num)
+  if num <= 1
+      return false
+    end
+
+    (2...num).each do |integer|
+      if (num % integer) == 0
+        return false
+      end
+    end
+
+    true
+  end
+#   if num>0 || num == 0 || num == 1
+#     return false
+#   else
+#     (2..num).to_a.all? do |integer|
+#       num % integer != 0
+#     end
+#   end
+# end
