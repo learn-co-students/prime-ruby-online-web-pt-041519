3f544d5157adf40bed7d816ae1a7ffdf3cd9e159
zhenggao cai <caizhengao1992@gmail.com>
2019-04-20 04:58:03 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(integer_number)
+array=[]
+array=(2..integer_number - 1).to_a
+if integer_number <= 1
+  return false 
+elsif  integer_number == 2
+  return true  
+elsif   
+array.any? {|number| integer_number % number == 0 }
+  return false 
+
+else
+  return true 
+end 
+end
