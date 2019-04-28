8794619f44f938d12c589397abfd9cf45ea0a93b
johncban <johncban@gmail.com>
2019-04-28 09:50:38 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+# Source: http://lortza.github.io/2017/09/22/prime-numbers-in-ruby.html
+def prime?(num)
+  if num > 1
+    r = (2..(num-1)).to_a
+    r.none? do |numtest|
+      num % numtest == 0
+    end
+  else
+    false
+  end
+end
